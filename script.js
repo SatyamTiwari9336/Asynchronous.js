4477dc987e7e7e3fa20aad65c616df4ce21d94f8
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-09-22 15:09:14 +0530

    new feature

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+/*
 const getcountry = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
@@ -36,8 +37,50 @@ const getcountry = function (country) {
   });
 };
 
-getcountry('bharat');
+getcountry('Bharat');
 getcountry('russia');
-ffg;
 getcountry('argentina');
 getcountry('sudan');
+*/
+
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}" >
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)}</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+      </article> `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+const getcountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    //render country
+    renderCountry(data);
+    //Get neighbouring Country
+    const [neighbour] = data.borders;
+    if (!neighbour) return;
+    //ajax call 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+getcountryAndNeighbour('Bharat');
