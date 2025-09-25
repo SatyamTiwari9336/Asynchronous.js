42e43287d7c0da1f69c7b4156e51b52d0d1409f3
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-09-25 15:52:15 +0530

    new

--- script.js
+++ script.js
@@ -200,3 +200,35 @@ console.log('test ends');
 
 ///////////////////////////////////////
 //building a simple promise
+const quicklottery = new Promise((resolve, reject) => {
+  console.log('lottery draw is happening ');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You won 💥💥');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
+});
+
+quicklottery.then(res => console.log(res)).catch(err => console.error(err));
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait
+  .then(() => {
+    console.log('i waited for 1 second');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('i waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => {
+    console.log(' i waited for 3 seconds ');
+    return wait(3);
+  });
