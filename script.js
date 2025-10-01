e679ba4e4034a8d8c1ccf1a39bdf2b77ceb97479
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-10-01 18:57:57 +0530

    parallel promises

--- script.js
+++ script.js
@@ -432,7 +432,7 @@ const get3countries = async function (c1, c2, c3) {
 };
 
 get3countries('portugal', 'Russia', 'bharat');
-*/
+
 /////////////////////////////////////////////////////////////////
 //promise.race
 (async function () {
@@ -476,3 +476,55 @@ Promise.any([
 
 /////////////////////////////////////////////
 //challenge - 3
+*/
+// PART 1
+// 1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+// PART 2
+// 2. Comsume the promise using .then and also add an error handler;
+// 3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+// 4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+// 5. After the second image has loaded, pause execution for 2 seconds again;
+// 6. After the 2 seconds have passed, hide the current image.
+const imgcontainer = document.querySelector('.images');
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const createImage = function (imgUrl) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgUrl;
+    img.addEventListener('load', function () {
+      imgcontainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener('error', function () {
+      reject(new Error('image not found'));
+    });
+  });
+};
+
+const Loadnpause = async function () {
+  try {
+    let img = await createImage('img/img-1.jpg');
+    console.log('image 1 is loaded ');
+    await wait(2);
+    img.style.display = 'none';
+
+    img = await createImage('img/img-2.jpg');
+    console.log('image 2 is loaded ');
+    await wait(2);
+    img.style.display = 'none';
+
+    img = await createImage('img/img-3.jpg');
+    console.log('image 3 is loaded ');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.log(err);
+  }
+};
+Loadnpause();
